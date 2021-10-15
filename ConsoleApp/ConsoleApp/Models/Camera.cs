4df51110bf8bc93933890b6537543d6dfdc6d43b
yinchang0626 <yinchang0626@gmail.com>
2021-10-15 11:42:25 +0800

    modify Camera model

--- ConsoleApp/ConsoleApp/Models/Camera.cs
+++ ConsoleApp/ConsoleApp/Models/Camera.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp.Models
 {
-    public class Camera
+    public class Camera : IEquatable<Camera>,IEqualityComparer<Camera>, IComparable<Camera>
     {
         public string 編號 { get; set; }
         public string 型式 { get; set; }
@@ -24,6 +25,36 @@ namespace ConsoleApp.Models
         {
 
             this.Datas = new Dictionary<string, string>();
+        }
+        
+
+        public bool Equals(Camera other)
+        {
+            return other.編號 == this.編號;
+        }
+        public override string ToString()
+        {
+            var r1 = base.ToString();
+            return $"行政區 :{this.行政區} 編號 :{this.編號} 地點:{this.測照地點} 速限:{this.速限}";
+        }
+        public override int GetHashCode()
+        {
+            return int.Parse(this.編號);
+        }
+
+        public bool Equals(Camera x, Camera y)
+        {
+            return x.編號 == y.編號;
+        }
+
+        public int GetHashCode([DisallowNull] Camera obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        public int CompareTo(Camera other)
+        {
+            return int.Parse(this.編號).CompareTo(int.Parse(other.編號));
         }
     }
 }
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleApp.Utils;
+﻿using ConsoleApp.Models;
+using ConsoleApp.Utils;
 using System;
 using System.Linq;
 
@@ -11,6 +12,7 @@ namespace ConsoleApp
             string fullFileName = FilePath.GetFullPath("高雄市交通取締.csv");
 
             var csvService = new ConsoleApp.Services.ImportCsvService();
+
             var csvDatas = csvService.LoadFormFile(fullFileName);
 
             Console.WriteLine(string.Format("分析完成，共有{0}筆資料", csvDatas.Count));
@@ -23,14 +25,13 @@ namespace ConsoleApp
                 Console.WriteLine($"行政區 :{x.Key} 數量:{x.ToList().Count}");
                 items.ForEach(x =>
                 {
-                    Console.WriteLine($"行政區 :{x.行政區} 編號 :{x.編號} 地點:{x.測照地點} 速限:{x.速限}");
+                    Console.WriteLine(x.ToString());
                 });
             });
 
-            //csvDatas.ForEach(x =>
-            //{
-            //    Console.WriteLine($"編號 :{x.編號} 地點:{x.測照地點} 速限:{x.速限}");
-            //});
+            csvDatas.Sort();
+
+
 
             Console.ReadKey();
 
