4c3579b5a1ba216d9f8ac3ed48530b8dbc2568d4
yinchang0626 <yinchang0626@gmail.com>
2021-11-19 08:23:18 +0800

    add DataService

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,40 +7,62 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var csvService1 = new ImportCsvService();
-
-            var csvDatas = csvService1.LoadFormFile(Utils.FilePath.GetFullPath("高雄市交通取締.csv"));
-
-            Console.WriteLine(string.Format("分析完成，共有{0}筆資料", csvDatas.Count));
-            csvDatas.ForEach(x =>
-            {
-                Console.WriteLine($"編號 :{x.編號} 地點:{x.測照地點} 速限:{x.速限}");
-            });
-            Console.ReadKey();
-
-            var xmlService1 = new ImportXmlService();
-
-            var datas = xmlService1.LoadFormFile(Utils.FilePath.GetFullPath("北捷站點.xml"));
-
-            Console.WriteLine(string.Format("分析完成，共有{0}筆資料", datas.Count));
-            datas.ForEach(x =>
-            {
-                Console.WriteLine(string.Format("編號 :{0} 名稱:{1}({2}) 描述:{3}", x.StationID, x.StationName, x.ExitName, x.LocationDescription));
-            });
-
-            Console.ReadKey();
-
             var jsonService = new ImportJsonService();
+            DataService dataService = new DataService();
 
             var jsonDatas = jsonService.LoadFormFile(Utils.FilePath.GetFullPath("高雄活動.txt"));
 
-            Console.WriteLine(string.Format("分析完成，共有{0}筆資料", jsonDatas.Count));
-            jsonDatas.ForEach(x =>
+            jsonDatas.ForEach(i =>
+            {
+                dataService.Insert(i);
+            });
+
+
+            
+            var rows=dataService.GetActivities();
+
+            Console.WriteLine(string.Format("分析完成，共有{0}筆資料", rows.Count));
+            rows.ForEach(x =>
             {
                 Console.WriteLine(string.Format("編號 :{0} 名稱:{1} 地點:{2}", x.PrgId, x.PrgName, x.OrgName));
             });
 
             Console.ReadKey();
+
+            //var csvService1 = new ImportCsvService();
+
+            //var csvDatas = csvService1.LoadFormFile(Utils.FilePath.GetFullPath("高雄市交通取締.csv"));
+
+            //Console.WriteLine(string.Format("分析完成，共有{0}筆資料", csvDatas.Count));
+            //csvDatas.ForEach(x =>
+            //{
+            //    Console.WriteLine($"編號 :{x.編號} 地點:{x.測照地點} 速限:{x.速限}");
+            //});
+            //Console.ReadKey();
+
+            //var xmlService1 = new ImportXmlService();
+
+            //var datas = xmlService1.LoadFormFile(Utils.FilePath.GetFullPath("北捷站點.xml"));
+
+            //Console.WriteLine(string.Format("分析完成，共有{0}筆資料", datas.Count));
+            //datas.ForEach(x =>
+            //{
+            //    Console.WriteLine(string.Format("編號 :{0} 名稱:{1}({2}) 描述:{3}", x.StationID, x.StationName, x.ExitName, x.LocationDescription));
+            //});
+
+            //Console.ReadKey();
+
+            //var jsonService = new ImportJsonService();
+
+            //var jsonDatas = jsonService.LoadFormFile(Utils.FilePath.GetFullPath("高雄活動.txt"));
+
+            //Console.WriteLine(string.Format("分析完成，共有{0}筆資料", jsonDatas.Count));
+            //jsonDatas.ForEach(x =>
+            //{
+            //    Console.WriteLine(string.Format("編號 :{0} 名稱:{1} 地點:{2}", x.PrgId, x.PrgName, x.OrgName));
+            //});
+
+            //Console.ReadKey();
 
         }
     }
---
+++ ConsoleApp/ConsoleApp/Services/DataService.cs
@@ -0,0 +1,114 @@
+﻿using ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Services
+{
+    public class DataService
+    {
+        private string _connectionString = @"Server=(LocalDb)\MSSQLLocalDB;Database=NKUST;Trusted_Connection=True";
+
+
+        public List<Activity> GetActivities()
+        {
+            var result = new List<Activity>();
+            SqlConnection cn = new SqlConnection(_connectionString);
+
+            cn.Open();
+
+            SqlCommand command = new SqlCommand("Select * from Activity", cn);
+            
+            var indexer = command.ExecuteReader();
+
+
+            while (indexer.Read())
+            {
+                var id = indexer["Id"];
+                var prgName = indexer["PrgName"];
+                var prgDate = indexer["PrgDate"];
+                var prgPlace = indexer["PrgPlace"];
+                var price = indexer["Price"];
+                var item = new Activity()
+                {
+                    PrgName = prgName.ToString(),
+                    PrgDate = prgDate.ToString(),
+                    PrgPlace = prgPlace.ToString(),
+                    Id = long.Parse(id.ToString())
+                };
+                result.Add(item);
+
+            }
+
+            cn.Close();
+
+            return result;
+        }
+
+
+        public void Insert(Activity activity)
+        {
+
+            var commandString = @"
+INSERT INTO [dbo].[Activity] ([PrgName],[PrgDate],[PrgPlace]) 
+VALUES (@PrgName,@PrgDate,@PrgPlace)";
+
+            SqlConnection cn = new SqlConnection(_connectionString);
+
+            cn.Open();
+
+            SqlCommand command = new SqlCommand(commandString, cn);
+
+            command.Parameters.Add(new SqlParameter("PrgName", activity.PrgName));
+            command.Parameters.Add(new SqlParameter("PrgDate", activity.PrgDate));
+            command.Parameters.Add(new SqlParameter("PrgPlace", activity.PrgPlace));
+
+            var count = command.ExecuteNonQuery();
+
+            cn.Close();
+        }
+
+        public void Update(Activity activity)
+        {
+            var commandString = @"
+UPDATE [dbo].[Activity]
+SET [PrgName] = ''
+      ,[PrgDate] =''
+      ,[PrgPlace] =''
+      ,[Price] = 0
+WHERE Id=1";
+
+            SqlConnection cn = new SqlConnection(_connectionString);
+
+            cn.Open();
+
+            SqlCommand command = new SqlCommand(commandString, cn);
+
+            var count = command.ExecuteNonQuery();
+
+            cn.Close();
+        }
+
+        public void Delete(int id)
+        {
+
+            var commandString = @"
+DELETE FROM [dbo].[Activity]
+WHERE Id=0";
+
+            SqlConnection cn = new SqlConnection(_connectionString);
+
+            cn.Open();
+
+            SqlCommand command = new SqlCommand(commandString, cn);
+
+            var count = command.ExecuteNonQuery();
+
+            cn.Close();
+        }
+
+    }
+}
