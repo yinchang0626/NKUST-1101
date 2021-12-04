1726fffaf64e1a2f877bdd762ffc18d9a2fef288
yinchang0626 <yinchang0626@gmail.com>
2021-12-04 14:23:02 +0800

    add ActivityController

---
+++ WebMvc/Controllers/ActivityController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Data;
+
+namespace WebMvc.Controllers
+{
+    public class ActivityController : Controller
+    {
+        protected ApplicationDbContext _applicationDbContext { get; }
+
+        public ActivityController(
+            ApplicationDbContext applicationDbContext
+            )
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+
+            var datas=_applicationDbContext.Activities
+                .ToList();
+
+
+            return View(datas);
+        }
+    }
+}
